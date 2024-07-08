744d37323164bc0612b02120b9844c368921d572
gvk <venkatakousikcse01@gmail.com>
2024-07-08 21:18:58 +0530

    shidf

--- controllers/userStatsController.js
+++ controllers/userStatsController.js
@@ -1,4 +1,6 @@
 import problemsProgress from "../models/problemsProgress.js";
+import dsaTracker from "../models/problemSchema.js";
+
 export const completeUserStats = async (req, res) => {
     try {
         const { userId } = req.body;
@@ -44,8 +46,6 @@ export const completeUserStats = async (req, res) => {
         totalProblemsSolved = userProblems.solvedProblems.length;
         totalProblemsForRevision = userProblems.revisionProblems.length;
 
-        // Assuming that you have some way to determine the difficulty level of the problems
-
         const stats = {
             totalProblems,
             totalFavourites,
@@ -60,3 +60,65 @@ export const completeUserStats = async (req, res) => {
     }
 };
 
+export const topicWiseStats = async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        let userProblems = await problemsProgress.findOne({ userId });
+        if (!userProblems) {
+            userProblems = new problemsProgress({
+                userId,
+                bookmarks: [],
+                favourites: [],
+                notes: new Map(),
+                solutions: new Map(),
+                solvedProblems: [],
+                unsolvedProblems: [],
+                revisionProblems: []
+            });
+
+            await userProblems.save();
+        }
+
+        const solvedProblems = userProblems.solvedProblems;
+
+        const topicWiseCount = {};
+        const easyProblems = [];
+        const mediumProblems = [];
+        const hardProblems = [];
+
+        for (const problemId of solvedProblems) {
+            const problem = await dsaTracker.findOne({ _id: problemId });
+            if (problem) {
+
+                if (topicWiseCount[problem.Topic]) {
+                    topicWiseCount[problem.Topic]++;
+                } else {
+                    topicWiseCount[problem.Topic] = 1;
+                }
+
+                const problemInfo = { id: problem._id, title: problem.Problem };
+                if (problem.Difficulty === 'Easy') {
+                    easyProblems.push(problemInfo);
+                } else if (problem.Difficulty === 'Medium') {
+                    mediumProblems.push(problemInfo);
+                } else if (problem.Difficulty === 'Hard') {
+                    hardProblems.push(problemInfo);
+                }
+            }
+        }
+
+        const stats = {
+            topicWiseCount,
+            problemsByDifficulty: {
+                Easy: easyProblems,
+                Medium: mediumProblems,
+                Hard: hardProblems,
+            }
+        };
+
+        return res.status(200).json({ stats });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
--- routes/userStatsRoute.js
+++ routes/userStatsRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { completeUserStats } from '../controllers/userStatsController.js';
+import { completeUserStats, topicWiseStats } from '../controllers/userStatsController.js';
 import authenticateToken from '../middlewares/authenticateToken.js';
 
 const router = express.Router();
 
 router.post("/completeUserStats", authenticateToken, completeUserStats);
+router.post("/topicWiseStats", authenticateToken, topicWiseStats);
 
 export default router;
